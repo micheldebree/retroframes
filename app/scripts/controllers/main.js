dcc54cd8e8ada2bb89473608f815a5212606731e
Michel de Bree <michel@micheldebree.nl>
2016-01-06 13:35:31 +0100

    Initial commit
    Generated scaffolding using ‘yo angular’

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name retroframesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the retroframesApp
+ */
+angular.module('retroframesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
