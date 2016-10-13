51d7061ea299080d582b95b632803354fcc9a538
Michel de Bree <michel@micheldebree.nl>
2016-10-13 16:41:48 +0200

    Refactored conversion code into Converter

--- Converter.js
+++ Converter.js
@@ -1,7 +1,11 @@
 // Convert a JIMP picture to a PixelImage 
 var PixelCalculator = require('./retropixels/PixelCalculator.js'),
   Remapper = require('./retropixels/Remapper.js'),
-  OrderedDitherers = require('./retropixels/OrderedDitherers.js');
+  OrderedDitherers = require('./retropixels/OrderedDitherers.js'),
+  fs = require('fs-extra'),
+  VideoTool = require('./VideoTool.js'),
+  ProgressBar = require('progress'),
+  Jimp = require('jimp');
 
 function convertImage(jimpImage, graphicMode) {
   var x,
@@ -27,6 +31,64 @@ function convertImage(jimpImage, graphicMode) {
   jimpImage.resize(pixelImage.width * pixelImage.pWidth, pixelImage.height * pixelImage.pHeight);
 }
 
+function findAllFiles(directory, callback) {
+    fs.readdir(directory, function(error, files) {
+        if (error) throw error;
+        console.log(files.length + " frames found in " + directory);
+        callback(files);
+    });
+}
+
+function convert(filenames, index, graphicMode, callback) {
+    if (index < filenames.length) {
+        bar.tick();
+        Jimp.read(filenames[index], function(err, image) {
+            if (err) throw err;
+            convertImage(image, graphicMode);
+            image.write(filenames[index]);
+            convert(filenames, index + 1, graphicMode, callback);
+        });
+    } else {
+        callback();
+    }
+}
+
+function convertFiles(tmpDir, files, graphicMode, callback) {
+    bar = new ProgressBar('Pixelating... [:bar] :percent frame :current/:total :etas', {
+        total: files.length
+    });
+    // prepend tmp dir
+    files = files.map(function(file) {
+        return tmpDir + '/' + file;
+    });
+
+    if (files.length > 0) {
+        console.log("Converting " + files.length + " files");
+        convert(files, 0, graphicMode, function() {
+            console.log("Done converting.");
+            callback();
+        });
+    } else {
+        throw new Error("No frames found to process.");
+    }
+}
+
+// extract frames and convert them to a graphicMode
+function convertVideo(filename, graphicMode, fps, callback) {
+    fs.mkdtemp('./tmp-', function(error, tmpDir) {
+        if (error) throw error;
+        console.log("Created temporary directory " + tmpDir);
+        retroPicture = graphicMode.create();
+        VideoTool.extractFrames(filename, tmpDir, fps, retroPicture, function() {
+            findAllFiles(tmpDir, function(files) {
+                convertFiles(tmpDir, files, graphicMode, function() {
+                callback(tmpDir);
+                });
+            });
+        });
+    });
+}
+
 module.exports = {
-  convertImage: convertImage
+  convertVideo: convertVideo
 };
--- index.js
+++ index.js
@@ -12,7 +12,6 @@ var PixelCalculator = require('./retropixels/PixelCalculator.js');
 var Remapper = require('./retropixels/Remapper.js');
 var OrderedDitherers = require('./retropixels/OrderedDitherers');
 var ErrorDiffusionDitherers = require('./retropixels/ErrorDiffusionDitherers');
-var ProgressBar = require('progress');
 var VideoTool = require('./VideoTool.js'),
     Converter = require('./Converter.js');
 var bar;
@@ -37,48 +36,6 @@ function silentDelete(filename, callback) {
     });
 }
 
-function convert(filenames, index, callback) {
-    if (index < filenames.length) {
-        bar.tick();
-        Jimp.read(filenames[index], function(err, image) {
-            if (err) throw err;
-            Converter.convertImage(image, graphicMode);
-            image.write(filenames[index]);
-            convert(filenames, index + 1, callback);
-        });
-    } else {
-        callback();
-    }
-}
-
-function convertFiles(tmpDir, files, callback) {
-    bar = new ProgressBar('Pixelating... [:bar] :percent frame :current/:total :etas', {
-        total: files.length
-    });
-    // prepend tmp dir
-    files = files.map(function(file) {
-        return tmpDir + '/' + file;
-    });
-
-    if (files.length > 0) {
-        console.log("Converting " + files.length + " files");
-        convert(files, 0, function() {
-            console.log("Done converting.");
-            callback();
-        });
-    } else {
-        throw new Error("No frames found to process.");
-    }
-}
-
-function findAllFiles(directory, callback) {
-    fs.readdir(directory, function(error, files) {
-        if (error) throw error;
-        console.log(files.length + " frames found in " + directory);
-        callback(files);
-    });
-}
-
 function cleanup(tmpDir, callback) {
     silentDelete('out.mp4', function() {
         console.log("Removing " + tmpDir);
@@ -86,6 +43,7 @@ function cleanup(tmpDir, callback) {
     });
 }
 
+// combine frames from a folder into a movie
 function makeMovie(tmpDir, callback) {
     VideoTool.combineFrames('out.mp4', tmpDir, fps, function() {
         VideoTool.muxAudio('final.mp4', 'out.mp4', 'in.mp4', function() {
@@ -94,25 +52,12 @@ function makeMovie(tmpDir, callback) {
     });
 }
 
-function convertVideo(filename, callback) {
-    fs.mkdtemp('./tmp-', function(error, tmpDir) {
-        if (error) throw error;
-        console.log("Created temporary directory " + tmpDir);
-        retroPicture = graphicMode.create();
-        VideoTool.extractFrames(filename, tmpDir, fps, retroPicture, function() {
-            findAllFiles(tmpDir, function(files) {
-                convertFiles(tmpDir, files, function() {
-                callback(tmpDir);
-                });
-            });
-        });
-    });
-}
+
 
 silentDelete('tmp.mp4', function() {
     silentDelete('out.mp4', function() {
         silentDelete('final.mp4', function() {
-            convertVideo('in.mp4', function(tmpDir) {
+            Converter.convertVideo('in.mp4', graphicMode, fps, function(tmpDir) {
                 makeMovie(tmpDir, function() {
                     cleanup(tmpDir, function() {
                         console.log('Done.');
